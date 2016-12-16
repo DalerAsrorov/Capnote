17869611e219b67d13b4e26f4dcab1013890cefc
Daler Asrorov <asrorids@gmail.com>
2016-12-15 20:25:18 -0800

    Created initial project for the app

---
+++ Capnote/CapnoteTests/CapnoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  CapnoteTests.swift
+//  CapnoteTests
+//
+//  Created by Daler Asrorov on 12/15/16.
+//  Copyright © 2016 Daler Asrorov. All rights reserved.
+//
+
+import XCTest
+@testable import Capnote
+
+class CapnoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
