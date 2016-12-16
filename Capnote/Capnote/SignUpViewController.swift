5f7b04da1962620da233788e8c5b453a6ebb4392
Daler Asrorov <asrorids@gmail.com>
2016-12-16 12:06:26 -0800

    added sign up layout

--- Capnote/Capnote/SignUpViewController.swift
+++ Capnote/Capnote/SignUpViewController.swift
@@ -1,28 +1,25 @@
 //
-//  LoginViewController.swift
+//  SignUpViewController.swift
 //  Capnote
 //
-//  Created by Daler Asrorov on 12/15/16.
+//  Created by Daler Asrorov on 12/16/16.
 //  Copyright © 2016 Daler Asrorov. All rights reserved.
 //
 
 import UIKit
-import FirebaseDatabase
 
-class LoginViewController: UIViewController {
-    @IBOutlet weak var usernameTF: UITextField!
-    @IBOutlet weak var passwordTF: UIView!
-    @IBOutlet weak var signInBtn: UIButton!
-    @IBOutlet weak var registerBtn: UIButton!
+class SignUpViewController: UIViewController {
     
-    var ref: FIRDatabaseReference!
+    @IBOutlet weak var newUsernameTF: UITextField!
+    @IBOutlet weak var newEmailTF: UITextField!
+    @IBOutlet weak var newPasswordTF: UITextField!
+    @IBOutlet weak var schoolNameTF: UITextField!
+    @IBOutlet weak var newMajorTF: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ref = FIRDatabase.database().reference()
-        
-        
-        
+
+
         // Do any additional setup after loading the view.
     }
 
@@ -31,11 +28,10 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func loginUser(_ sender: Any) {
+    @IBAction func signUpUser(_ sender: Any) {
+        
     }
 
-    @IBAction func registerUser(_ sender: Any) {
-    }
     /*
     // MARK: - Navigation
 
